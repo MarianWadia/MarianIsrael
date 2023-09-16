58d589e14901b5fab5ee7e3423d414686c39ddad
Marian Israel <marianwadia55@gmail.com>
2023-09-16 20:44:30 +0200

    added about section

---
+++ src/components/About.jsx
@@ -0,0 +1,32 @@
+
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+            <div className="pb-8 mt-20">
+                <h3 className="border-b-4 border-gray-500 inline text-4xl">About</h3>
+            </div>
+            <p className="text-l mt-10">
+            I am a hardworking junior full-stack developer with experience in both front-end and back-end development. I have expertise in HTML,
+            CSS, JavaScript, React.js, and Next.js for building responsive and user-friendly web interfaces. On the back end, 
+            I have worked with Node.js, Express, and PostgreSQL to develop RESTful APIs and manage databases. 
+            I am also familiar with Git version control and agile development methodologies. Additionally, my experience with programming languages 
+            such as C++, Python, Java, and PHP has provided me with a diverse knowledge base to advance my coding skills 
+            and my career in the tech industry.
+            </p>
+
+            <br />
+
+            <p className="text-l">
+                I am passionate about learning new technologies and improving my skills to create high-quality, scalable, and maintainable web applications. 
+                I am a quick learner, detail-oriented, and possess strong problem-solving skills. I enjoy collaborating with other developers and 
+                designers to deliver innovative solutions that meet business requirements and user needs. I am always eager to expand 
+                my skills and contribute to meaningful projects that make a positive impact on the industry, 
+                furthering my career as a software developer.    
+            </p>
+        </div>  
+    </div>
+  )
+}
+
+export default About
