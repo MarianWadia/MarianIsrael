6fd595434b10acc8b6c8aa33194ce70ddcad191d
Marian Israel <marianwadia55@gmail.com>
2023-09-17 21:42:23 +0200

    edited font in heading

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
     <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
         <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
             <div className="pb-8 mt-20">
-                <h3 className="border-b-4 border-gray-500 inline text-4xl">About</h3>
+                <h3 className="border-b-4 font-bold border-gray-500 inline text-4xl">About</h3>
             </div>
             <p className="text-l mt-10">
             I am a hardworking junior full-stack developer with experience in both front-end and back-end development. I have expertise in HTML,
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -49,18 +49,18 @@ const Portfolio = () => {
     <div name="portfolio" className="w-full bg-gradient-to-b from-black to-gray-800 text-white">
         <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
             <div className="pb-8 mt-20">
-                <h3 className="border-b-4 border-gray-500 inline text-4xl">Portfolio</h3>
+                <h3 className="border-b-4 font-bold border-gray-500 inline text-4xl">Portfolio</h3>
                 <h5 className="py-6">check out some of my work right here</h5>
             </div>
 
-            <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-4 sm:px-0">
+            <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
                 {portfolios.map((portfolio)=>(
                     <div className="shadow-md shadow-gray-600 rounded-lg" key={portfolio.id}>
                         <img src={portfolio.src} alt="" 
-                            className="rounded-md duration-200 hover:scale-105"/>
+                            className="rounded-md duration-500 hover:scale-105"/>
                         <div className="flex items-center justify-between">
-                            <button onClick= {()=>onButtonClick(portfolio.demoLink)} className="w-1/2 duration-200 hover:bg-slate-500 p-4">Demo Link</button>
-                            <button onClick= {()=>onButtonClick(portfolio.codeLink)} className="w-1/2 duration-200 hover:bg-slate-500 p-4">Code</button>
+                            <button onClick= {()=>onButtonClick(portfolio.demoLink)} className="w-1/2 duration-500 hover:bg-slate-500 p-4">Demo Link</button>
+                            <button onClick= {()=>onButtonClick(portfolio.codeLink)} className="w-1/2 duration-500 hover:bg-slate-500 p-4">Code</button>
                         </div>
                     </div>
                 ))}
