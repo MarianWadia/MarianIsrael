07c820f6f721baea36fabae10b2d900b697d69cb
Marian Israel <marianwadia55@gmail.com>
2023-09-18 18:33:47 +0200

    added navbar smooth scrolling in all screens

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import {FaBars, FaTimes} from 'react-icons/fa'
+import { Link } from 'react-scroll'
 
 const NavBar = () => {
     const [nav, setNav] = useState(false)
     const [navLinks, setNavLinks] = useState(false)
+    const [hidden, setHidden] = useState(false);
     const links = [
         {
             id: 1,
@@ -26,6 +28,11 @@ const NavBar = () => {
             text: "contact",
         }
     ]
+    const onNavClickMobile = () => {
+        setNavLinks(false);
+        setNav(!nav);
+      }
+    
   return (
     <div className='flex justify-between fixed h-20 items-center w-full text-white bg-black px-4'>
         <div>
@@ -33,7 +40,9 @@ const NavBar = () => {
         </div>
         <ul className='hidden md:flex gap-4 '>
             {links.map(link =>
-                <li key={link.id} className='cursor-pointer capitalize text-gray-500 font-medium hover:scale-105 duration-200'>{link.text}</li>
+                <li key={link.id} className='cursor-pointer capitalize text-gray-500 font-medium hover:scale-105 duration-200'>
+                    <Link to={link.text} smooth duration={500}>{link.text}</Link>
+                </li>
             )}
         </ul>
 
@@ -45,19 +54,18 @@ const NavBar = () => {
             )}
         </div>
         {navLinks&&(
-            <ul className='md:hidden lg:hidden flex flex-col justify-center items-center absolute top-0 left-0 w-full 
-                h-screen bg-gradient-to-b from-black to-gray-800 gap-20'>
+            <ul className={`md:hidden lg:hidden flex flex-col justify-center items-center absolute top-0 left-0 w-full 
+                h-screen bg-gradient-to-b from-black to-gray-800 gap-20`}>
                 {links.map(link =>
                     <li key={link.id} className='cursor-pointer capitalize text-gray-500 text-4xl 
                     font-medium hover:scale-105 duration-200 w-full text-center'
                     >
-                        {link.text}
+                        <Link to={link.text} smooth duration={500} onClick={onNavClickMobile}>{link.text}</Link>
                     </li>
                 )}
             </ul>
         )}
         
-
     </div>
   )
 }
