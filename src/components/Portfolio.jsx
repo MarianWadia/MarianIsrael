c652c3425f3a4c135d263b0353841af4b2c6a903
Marian Israel <marianwadia55@gmail.com>
2023-10-15 17:26:20 +0200

    added carcoach && udagram hosting porjects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,6 +1,7 @@
 import RealEstates from "../assets/Picture2.png"
 import quizApp from "../assets/quizApp.png"
 import delicousApp from "../assets/delicousApp.png"
+import carcoach from "../assets/carcoach.png"
 
 const Portfolio = () => {
     const portfolios =[
@@ -37,6 +38,16 @@ const Portfolio = () => {
             id: 6,
             src: "https://media.licdn.com/dms/image/sync/D4D27AQFqrAxn5T_yTA/articleshare-shrink_800/0/1696350703516?e=1697572800&v=beta&t=31aAZbaL53D9CwJ0x93aouemowHGrBuSqV9Kmf-e6Tk",
             codeLink: "https://github.com/MarianWadia/Udacity-storefrontApi"
+        },
+        {
+            id: 7,
+            src: carcoach,
+            codeLink: "https://github.com/MarianWadia/CarCoach-GradProject"
+        },
+        {
+            id: 8,
+            src: "https://github.com/MarianWadia/HostingFullstackApp/raw/master/DOCUMENTS/screenshots/circleci-success.png",
+            codeLink: "https://github.com/MarianWadia/HostingFullstackApp"
         }
     ]
     const onButtonClick = (pressedLink)=>{
@@ -54,13 +65,12 @@ const Portfolio = () => {
             <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
                 {portfolios.map((portfolio)=>(
                     <div className="shadow-md shadow-gray-600 rounded-lg" key={portfolio.id}>
-                        <img src={portfolio.src} alt="" 
-                            className="rounded-md duration-500 w-full h-3/4 hover:scale-105"/>
+                        <img src={portfolio.src} alt="" className="rounded-md duration-500 w-full h-3/4 max-h-fit hover:scale-110"/>
                         <div className="flex items-center justify-between h-1/4">
                         { portfolio.demoLink && (
-                            <button onClick= {()=>onButtonClick(portfolio.demoLink)} className="w-1/2  duration-300 hover:bg-slate-800 hover:opacity-80 p-4 rounded-md">Demo Link</button>
+                            <button onClick= {()=>onButtonClick(portfolio.demoLink)} className="w-1/2 duration-300 hover:bg-slate-800 hover:opacity-80 p-4 rounded-md text-xs md:text-lg">Demo Link</button>
                         )}
-                            <button onClick= {()=>onButtonClick(portfolio.codeLink)} className={`${portfolio.demoLink ? "w-1/2" : "w-full text-lg"}  hover:bg-slate-800 hover:opacity-80 p-4 rounded-md`}>Source Code</button>
+                            <button onClick= {()=>onButtonClick(portfolio.codeLink)} className={`${portfolio.demoLink ? "w-1/2 text-xs md:text-lg" : "w-full text-sm md:text-lg "}  hover:bg-slate-800 hover:opacity-80 p-4 rounded-md`}>Source Code</button>
                         </div>
                     </div>
                 ))}
